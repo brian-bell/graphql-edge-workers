387a00ef2c53112b743ea6c5d12f757ee5952197
Brian Bell <brian-bell@users.noreply.github.com>
2026-03-12 20:40:47 -0400

    Remove zero trust terraform setup (#4)

--- infra/terraform/cloudflare/main.tf
+++ infra/terraform/cloudflare/main.tf
@@ -15,30 +15,3 @@ resource "cloudflare_worker" "graphql" {
     enabled = var.enable_workers_dev_subdomain
   }
 }
-
-resource "cloudflare_zero_trust_access_policy" "allowed_user" {
-  account_id       = var.cloudflare_account_id
-  name             = "${var.worker_name}-${var.environment}-allow-user"
-  decision         = "allow"
-  session_duration = var.access_session_duration
-
-  include = [{
-    email = {
-      email = var.access_allowed_email
-    }
-  }]
-}
-
-resource "cloudflare_zero_trust_access_application" "worker" {
-  account_id = var.cloudflare_account_id
-  name       = "${var.worker_name}-${var.environment}"
-  type       = "self_hosted"
-  domain     = local.worker_domain
-
-  policies = [{
-    id         = cloudflare_zero_trust_access_policy.allowed_user.id
-    precedence = 1
-  }]
-
-  depends_on = [cloudflare_worker.graphql]
-}
